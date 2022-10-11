4d19cf1261001aead449060144a57442a1a547c0
Oli Goodall <Oli.Goodall@softwire.com>
2022-10-11 16:20:09 +0100

    WS-16: Add User function added

---
+++ backend/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using WhaleSpotting.Services;
+using WhaleSpotting.Helpers;
+using WhaleSpotting.Models.Request;
+using WhaleSpotting.Models.Database;
+using WhaleSpotting.Models.Response;
+
+namespace WhaleSpotting.Controllers {
+    [ApiController]
+    [Route("/users")]
+    public class UserController : ControllerBase {
+
+        private readonly IAuthService _authService;
+        private readonly IUserService _userService;
+
+        public UserController
+        (
+            IAuthService authService,
+            IUserService userService
+        ) {
+            _authService = authService;
+            _userService = userService;
+        }
+
+        [HttpPost("create")]
+        public ActionResult<CreateUserResponse> Create([FromHeader] string authorization, [FromBody] CreateUserRequest newUserRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (authorization is null)
+            {
+                return new UnauthorizedResult();
+            }
+
+            try
+            {
+                var encodedUsernamePassword = authorization.Substring("Basic ".Length).Trim();
+                string usernamePassword = AuthHelper.Base64Decode(encodedUsernamePassword);
+                int separatorIndex = usernamePassword.IndexOf(':');
+
+                var splitUsernamePassword = usernamePassword.Split(':');
+                var username = splitUsernamePassword[0];
+                var password = splitUsernamePassword[1];
+
+                var check = _authService.IsValidLoginInfo(username, password);
+                if (!check)
+                {
+                    return new UnauthorizedResult();
+                }
+                
+                User createdUser = _userService.Create(newUserRequest);
+
+                return new CreateUserResponse(newUserRequest, true);  
+            }
+
+            catch (Exception)
+            {
+                return new UnauthorizedResult();
+            }
+        }
+    }
+}
