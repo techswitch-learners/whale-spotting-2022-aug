f35b258c7c68000c8dcbb3bfc839271a9d8bef19
tomscambler <tomscambler@gmail.com>
2022-10-12 13:37:02 +0100

    WS-22: Added Sighting controller, repo and service, as well as ListResponse model

--- backend/Controllers/SightingController.cs
+++ backend/Controllers/SightingController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using WhaleSpotting.Services;
+using WhaleSpotting.Models.Response;
+using WhaleSpotting.Models.Database;
 
 namespace WhaleSpotting.Controllers {
     [ApiController]
@@ -7,11 +9,16 @@ namespace WhaleSpotting.Controllers {
     public class SightingController : ControllerBase {
         private readonly ISightingService _sightings;
 
-        public SightingController
-        (
-            ISightingService sightings
-        ) {
+        public SightingController(ISightingService sightings) 
+        {
             _sightings = sightings;
+        }
+
+        [HttpGet("/species/{speciesId}")]
+        public ActionResult<ListResponse<Sighting>> GetSightingsBySpeciesId([FromRoute] int speciesId)
+        {
+            var sightings = _sightings.GetSightingsBySpeciesId(speciesId);
+            return new ListResponse<Sighting>(sightings);
         }
     }
 }
--- backend/Repositories/SightingRepo.cs
+++ backend/Repositories/SightingRepo.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
+using WhaleSpotting.Models.Database;
+
 namespace WhaleSpotting.Repositories
 {
     public interface ISightingRepo
     {
+        IEnumerable<Sighting> GetSightingsBySpeciesId(int speciesId);
     }
 
     public class SightingRepo : ISightingRepo
@@ -11,6 +16,13 @@ namespace WhaleSpotting.Repositories
         public SightingRepo(WhaleSpottingDbContext context)
         {
             _context = context;
+        }
+
+        public IEnumerable<Sighting> GetSightingsBySpeciesId(int speciesId)
+        {
+            return _context.Sightings
+                .Where(s => s.Species.Id == speciesId)
+                .OrderByDescending(s => s.SeenOn);
         }
     }
 }
