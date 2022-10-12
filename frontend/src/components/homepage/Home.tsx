c3ab816ac237654a66eb8043dcb5c6e1fac70336
Oli Goodall <Oli.Goodall@softwire.com>
2022-10-12 16:04:52 +0100

    Added some form validation

--- frontend/src/components/homepage/Home.tsx
+++ frontend/src/components/homepage/Home.tsx
@@ -1,19 +1,71 @@
 import React, { useState } from "react";
+import internal from "stream";
+import "./Home.scss";
 
 export const Home: React.FunctionComponent = () => {
   const [locationInputType, setLocationInputType] = useState("");
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [description, setDescription] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [dateError, setDateError] = useState("");
+  const [latError, setLatError] = useState("");
+  const [longError, setLongError] = useState("");
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setLocationInputType(event.target.value);
   };
 
+  const validateForm = (
+    name: string,
+    date: string,
+    imageUrl: string,
+    latitude: string,
+    longitude: string
+  ) => {
+    if (name === "") {
+      setNameError("Required field");
+    }
+    if (imageUrl.includes(" ")) {
+      setImageUrl("URL cannot contain spaces");
+    }
+    if (!date) {
+      setDateError("Please enter date");
+    }
+    if (!latitude) {
+      setLatError("Please enter a latitude");
+    }
+    if (!longitude) {
+      setLongError("Please enter a longitude");
+    }
+  };
+
+  const handleSubmit = () => {
+    validateForm(name, date, imageUrl, latitude, longitude);
+  };
+
   return (
-    <>
+    <div className="homePage">
       <h1>Whale Spotting</h1>
       <p>Spot whales!</p>
-      <form method="post">
-        <input type="text" placeholder="name"></input>
-        <input type="datetime-local"></input>
+      <form className="newSightingForm" method="post" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        ></input>
+        {nameError}
+        <input
+          type="datetime-local"
+          onChange={(e) => {
+            setDate(e.target.value);
+          }}
+        ></input>
         <ul>
           <li>
             <input
@@ -34,11 +86,27 @@ export const Home: React.FunctionComponent = () => {
             />
             Enter latitude and longitude
             {locationInputType === "manual" && (
-              <div>
-                <input type="number" min="-90" max="90" step="0.1" />
-                Latitude
-                <input type="number" min="-180" max="180" step="0.1" />
-                Longitude
+              <div className="latAndLongInputs">
+                <input
+                  type="number"
+                  placeholder="Latitude"
+                  min="-90"
+                  max="90"
+                  step="0.1"
+                  onChange={(e) => {
+                    setLatitude(e.target.value);
+                  }}
+                />
+                <input
+                  type="number"
+                  placeholder="Longitude"
+                  min="-180"
+                  max="180"
+                  step="0.1"
+                  onChange={(e) => {
+                    setLongitude(e.target.value);
+                  }}
+                />
               </div>
             )}
           </li>
@@ -53,9 +121,22 @@ export const Home: React.FunctionComponent = () => {
             Start typing a location
           </li>
         </ul>
-
+        <input
+          type="text"
+          placeholder="Description"
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+        ></input>
+        <input
+          type="text"
+          placeholder="Image Url"
+          onChange={(e) => {
+            setImageUrl(e.target.value);
+          }}
+        ></input>
         <button type="submit">Submit post</button>
       </form>
-    </>
+    </div>
   );
 };
