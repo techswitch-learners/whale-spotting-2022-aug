785ec72f49572492dfed6f43922e98d44cc6d578
tomscambler <tomscambler@gmail.com>
2022-10-13 12:28:09 +0100

    WS-10: started css

--- frontend/src/components/whaleopedia-page/Whaleopedia.tsx
+++ frontend/src/components/whaleopedia-page/Whaleopedia.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAllSpecies, ListResponse, Species } from "../../clients/apiClient";
+import "Whaleopedia.scss";
 
 export const Whaleopedia: React.FunctionComponent = () => {
-  return <></>;
+  const [listOfWhales, setListOfWhales] = useState<ListResponse<Species>>();
+
+  useEffect(() => {
+    getAllSpecies().then(setListOfWhales);
+  }, []);
+
+  if (listOfWhales === undefined) {
+    return <p>Loading</p>;
+  }
+  return (
+    <div>
+      {listOfWhales.items.map((whale) => {
+        return (
+          <div>
+            <img className="whale_info--image" src={whale.photoUrl} />
+
+            <h1 className="whale_info--common-name">{whale.name}</h1>
+            <h3 className="whale_info--scientific">{whale.scientificName}</h3>
+          </div>
+        );
+      })}
+      ;
+    </div>
+  );
 };
