8e80b50ee933a3b0ea9fcfb7341f4838ca65e0d7
Bharathi Ramachandran <Bharathi.Ramachandran@softwire.com>
2022-10-13 15:47:55 +0100

    WS-10: Did CSS for Whaleopedia

--- frontend/src/components/whaleopedia-page/WhaleCard.tsx
+++ frontend/src/components/whaleopedia-page/WhaleCard.tsx
@@ -15,11 +15,17 @@ export const WhaleCard: React.FC<WhaleCardProps> = ({ whale }) => {
   return (
     <div className="whale-list_card">
       <div className="whale-list_card--common-name">{whale.name}</div>
-
       <img className="whale-list_card--image" src={whale.photoUrl} />
-      <div className="whale-list_card--scientific"> {whale.scientificName}</div>
-      <div className="whale-list_card--description"> {whale.description}</div>
+      <div className="whale-list_card--scientific">
+        <strong>Scientific Name: </strong>
+        {whale.scientificName}
+      </div>
+      <div className="whale-list_card--description">
+        <strong>Description: </strong>
+        {whale.description}
+      </div>
       <div className="whale-list_card--conservation">
+        <strong>Conservation Status: </strong>
         {whale.conservationStatus.code}
       </div>
     </div>
