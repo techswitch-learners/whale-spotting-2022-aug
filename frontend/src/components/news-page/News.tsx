c2edbee804c0e96ed8cca32e08c17d9b89187b07
Leo Ng <Leo.Ng@softwire.com>
2022-10-13 11:18:56 +0100

    finished news feeder from reddit

--- frontend/src/components/news-page/News.tsx
+++ frontend/src/components/news-page/News.tsx
@@ -1,41 +1,19 @@
 import React, { useState, useEffect } from "react";
 
-interface NewsApiResponse {
-  status: string;
-  totalResults: number;
-  articles: Article[];
-}
-
-interface Article {
-  title: string;
-  author: string;
-  source: Source;
-  publishedAt: string;
-  url: string;
-}
-
-interface Source {
-  Id: string;
-  Name: string;
-}
-
 export const News: React.FunctionComponent = () => {
   const [articlesList, setArticlesList] = useState<any[]>([]);
-  const [isloading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const url = "https://www.reddit.com/r/whales/top.json?limit=50&t=month";
 
     const fetchData = async () => {
       try {
-        setIsLoading(true);
         const response = await fetch(url);
         const json: any = await response.json();
         const articles = json.data.children;
         setArticlesList(
           articles.filter((article: any) => article.data.is_video)
         );
-        setIsLoading(false);
       } catch (error) {
         console.log("error", error);
       }
@@ -49,7 +27,9 @@ export const News: React.FunctionComponent = () => {
       {articlesList &&
         articlesList.map((article) => (
           <>
-            <p key={article.data.Id}>{article.data.title}</p>
+            <p key={article.data.Id}>
+              {article.data.title.replace(/&amp;/g, "&")}
+            </p>
             <video
               src={article.data.media.reddit_video.fallback_url}
               width="320"
