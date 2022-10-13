52eea5b43906275d959c2618847da4d9b1f841a1
Leo Ng <Leo.Ng@softwire.com>
2022-10-13 13:33:57 +0100

    Added date to news feed page

--- frontend/src/components/news-page/News.tsx
+++ frontend/src/components/news-page/News.tsx
@@ -1,5 +1,41 @@
 import React, { useState, useEffect } from "react";
 
+interface RedditApiResponse {
+  data: ApiData;
+}
+
+interface ApiData {
+  children: ChildrenElement[];
+}
+
+interface ChildrenElement {
+  data: ParentListData;
+}
+
+interface ParentListData {
+  created: number;
+  title: string;
+  id: string;
+  media: MediaData;
+  is_video: boolean;
+}
+
+interface ParentListDataNew {
+  created: Date;
+  title: string;
+  id: string;
+  media: MediaData;
+  is_video: boolean;
+}
+
+interface MediaData {
+  reddit_video: VideoUrl;
+}
+
+interface VideoUrl {
+  fallback_url: string;
+}
+
 export const News: React.FunctionComponent = () => {
   const [articlesList, setArticlesList] = useState<any[]>([]);
 
@@ -9,11 +45,18 @@ export const News: React.FunctionComponent = () => {
     const fetchData = async () => {
       try {
         const response = await fetch(url);
-        const json: any = await response.json();
-        const articles = json.data.children;
-        setArticlesList(
-          articles.filter((article: any) => article.data.is_video)
+        const json: RedditApiResponse = await response.json();
+        const articles: ChildrenElement[] = json.data.children;
+        const arr: any = articles.filter(
+          (article: ChildrenElement) => article.data.is_video
         );
+
+        arr.forEach(
+          (item: any) =>
+            (item.data.created = new Date(item.data.created * 1000).toString())
+        );
+
+        setArticlesList(arr);
       } catch (error) {
         console.log("error", error);
       }
@@ -25,12 +68,14 @@ export const News: React.FunctionComponent = () => {
     <>
       <h1>Whale content</h1>
       {articlesList &&
-        articlesList.map((article) => (
+        articlesList.map((article: ChildrenElement) => (
           <>
-            <p key={article.data.Id}>
+            <p key={article.data.id}>
               {article.data.title.replace(/&amp;/g, "&")}
             </p>
+            <p key={article.data.id}>{article.data.created}</p>
             <video
+              key={article.data.media.reddit_video.fallback_url}
               src={article.data.media.reddit_video.fallback_url}
               width="320"
               height="320"
