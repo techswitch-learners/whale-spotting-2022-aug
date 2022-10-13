1dbd863cefc9b258b2369192677520016c9ad0d8
Konstantinos <kon.xen@:gmail.com>
2022-10-13 15:34:25 +0100

    WS-07: client should be ok

---
+++ frontend/src/clients/apiClient.ts
@@ -0,0 +1,20 @@
+﻿export interface ListResponse<T> {
+  items: T[];
+}
+
+export interface Sighting {
+  id: number;
+  species: string;
+  imageUrl: string;
+  description: string;
+  whaleCount: number;
+  confirmationStatus: string;
+  location: string;
+  latitude: number;
+  longitude: number;
+}
+
+export const getSightings = async (): Promise<ListResponse<Sighting>> => {
+  const response = await fetch(`https://localhost:5001/sightings`);
+  return await response.json();
+};
