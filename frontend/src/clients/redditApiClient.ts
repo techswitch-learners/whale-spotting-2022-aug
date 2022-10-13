9d66dad880250c4abf496b0a4e2582177d5ec4a8
Jack Hughes <Jack.Hughes@softwire.com>
2022-10-13 16:01:10 +0100

    WS-39: Added divs, HTML symbol handling and error handling

--- frontend/src/clients/redditApiClient.ts
+++ frontend/src/clients/redditApiClient.ts
@@ -35,8 +35,10 @@ interface VideoUrl {
 }
 
 export const getTopWhaleVideos = async (): Promise<RedditVideo[]> => {
+  // const url = "https://www.reddit.com/r/whales/top.jso";
   const url = "https://www.reddit.com/r/whales/top.json?limit=50&t=month";
   const response = await fetch(url);
+  console.log(response);
   const json: RedditApiResponse = await response.json();
   const articles: RedditVideoResponse[] = json.data.children;
   const arr: RedditVideoResponse[] = articles.filter(
--- frontend/src/components/news-page/News.tsx
+++ frontend/src/components/news-page/News.tsx
@@ -3,33 +3,51 @@ import { getTopWhaleVideos, RedditVideo } from "../../clients/redditApiClient";
 
 export const News: React.FunctionComponent = () => {
   const [articlesList, setArticlesList] = useState<RedditVideo[]>();
+  const [pageStatus, setPageStatus] = useState<boolean>(true);
 
   useEffect(() => {
     const getVideos = async () => {
-      const videos = await getTopWhaleVideos();
-      setArticlesList(videos);
+      try {
+        const videos = await getTopWhaleVideos();
+        setArticlesList(videos);
+      } catch (err) {
+        console.log("error", err);
+        setPageStatus(false);
+      }
     };
 
     getVideos();
   }, []);
 
-  return (
-    <>
-      <h1>Whale content</h1>
-      {articlesList &&
-        articlesList.map((article: RedditVideo) => (
-          <>
-            <p key={article.id}>{article.title.replace(/&amp;/g, "&")}</p>
-            {/* <p key={article.id}>{article.created}</p> */}
-            <video
-              key={article.media.reddit_video.fallback_url}
-              src={article.media.reddit_video.fallback_url}
-              width="320"
-              height="320"
-              controls
-            />
-          </>
-        ))}
-    </>
-  );
+  if (pageStatus == false) {
+    return (
+      <>
+        <h1>Whale content</h1>
+        <p>Error Message</p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>Whale content</h1>
+        {articlesList &&
+          articlesList?.map((article: RedditVideo) => (
+            <div className="videoCard" key={article.id}>
+              <p
+                key={article.id}
+                dangerouslySetInnerHTML={{ __html: article.title }}
+              ></p>
+              {/* <p key={article.id}>{article.created}</p> */}
+              <video
+                key={article.media.reddit_video.fallback_url}
+                src={article.media.reddit_video.fallback_url}
+                width="320"
+                height="320"
+                controls
+              />
+            </div>
+          ))}
+      </>
+    );
+  }
 };
