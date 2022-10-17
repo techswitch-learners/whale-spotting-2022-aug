c0abc00e3249de4a3d3a944f16101db86f36dde6
Jack Hughes <Jack.Hughes@softwire.com>
2022-10-17 14:33:22 +0100

    initial branch commit

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,10 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { LoginContext } from "../login/LoginManager";
 import "./Navbar.scss";
 
 export const Navbar: React.FunctionComponent = () => {
   const loginContext = useContext(LoginContext);
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  function toggleMenu() {
+    setIsExpanded((current) => !current);
+  }
 
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
@@ -15,15 +20,38 @@ export const Navbar: React.FunctionComponent = () => {
           alt="Whale Spotting logo"
         />
       </Link>
-      <div>
+      <Link to="/">
+        <h1>WHALESPOTTING</h1>
+      </Link>
+      <Link to="/brose-sightings">
+        <h1>BROWSE SIGHTINGS</h1>
+      </Link>
+      <Link to="/whaleopedia">
+        <h1>WHALEOPEDIA</h1>
+      </Link>
+      <Link to="/news">
+        <h1>VIDEOS</h1>
+      </Link>
+
+      <div className="admin-links">
         {!loginContext.isLoggedIn ? (
-          <div></div>
-        ) : (
-          <Link to="/">
-            <a className="button is-primary" onClick={loginContext.logOut}>
-              <strong>Logout</strong>
-            </a>
+          <Link to="/login">
+            <h1>LOG IN</h1>
           </Link>
+        ) : (
+          <div className="admin-only-links">
+            <Link to="/approve-sightings">
+              <h1>APPROVE SIGHTINGS</h1>
+            </Link>
+            <Link to="/create-user">
+              <h1>CREATE USER</h1>
+            </Link>
+            <Link to="/">
+              <a className="button is-primary" onClick={loginContext.logOut}>
+                <strong>LOG OUT</strong>
+              </a>
+            </Link>
+          </div>
         )}
       </div>
     </nav>
