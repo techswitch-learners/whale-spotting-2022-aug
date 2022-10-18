9725ae7c6aadb019bcbe96f755052bdf42e9d43d
Disha Pandit <Disha.Pandit@softwire.com>
2022-10-18 13:39:56 +0100

    WS-19: Everything working

--- backend/Controllers/SightingController.cs
+++ backend/Controllers/SightingController.cs
@@ -4,18 +4,20 @@ using WhaleSpotting.Models.Response;
 using WhaleSpotting.Models.Database;
 using WhaleSpotting.Models.Request;
 
-namespace WhaleSpotting.Controllers {
+namespace WhaleSpotting.Controllers
+{
     [ApiController]
     [Route("/sightings")]
-    public class SightingController : ControllerBase {
+    public class SightingController : ControllerBase
+    {
         private readonly ISightingService _sightings;
 
         public SightingController(ISightingService sightings)
         {
             _sightings = sightings;
         }
-        
-        [HttpGet] 
+
+        [HttpGet]
         public ActionResult<ListResponse<Sighting>> GetApprovedSightings()
         {
             var approvedSightings = _sightings.GetApprovedSightings();
@@ -27,20 +29,26 @@ namespace WhaleSpotting.Controllers {
         {
             var sightings = _sightings.GetSightingsBySpeciesId(speciesId);
             return new ListResponse<Sighting>(sightings);
-        }   
+        }
 
-        [HttpGet("/pending")] 
-        public ActionResult<ListResponse<Sighting>>GetPendingSightings()
+        [HttpGet("/pending")]
+        public ActionResult<ListResponse<Sighting>> GetPendingSightings()
         {
             var pendingSightings = _sightings.GetPendingSightings();
             return new ListResponse<Sighting>(pendingSightings);
-        }   
+        }
 
         [HttpPost]
         public ActionResult CreateSighting([FromBody] CreateSightingRequest createSightingRequest)
-        {   
+        {
             var createdSighting = _sightings.CreateSighting(createSightingRequest);
             return Created("/api", createdSighting);
+        }
+        [HttpGet("{sightingId}")]
+        public ActionResult<Sighting> GetSightingById([FromRoute] int sightingId)
+        {
+            var sighting = _sightings.GetSightingById(sightingId);
+            return sighting;
         }
     }
 }
