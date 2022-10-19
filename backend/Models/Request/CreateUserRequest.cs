e7a8bb80c3e258e9b8cf779d4d959cf2484cd533
Tim Leach <timleach635@gmail.com>
2022-10-19 14:50:10 +0100

    WS-50: Use Microsoft's disgusting email regex

--- backend/Models/Request/CreateUserRequest.cs
+++ backend/Models/Request/CreateUserRequest.cs
@@ -13,7 +13,11 @@ namespace WhaleSpotting.Models.Request
         public string Username { get; set; }
 
         [Required]
-        [EmailAddress]
+        [RegularExpression(
+            // Microsoft's recommended email address validation Regex (absolutely disgusting, I know)
+            @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))"
+            + @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$"
+        )]
         public string Email { get; set; }
 
         [Required]
