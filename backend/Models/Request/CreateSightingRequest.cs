ba874ab10b2571bf27ccda381acbeedc20a1338b
Tim Leach <timleach635@gmail.com>
2022-10-21 14:57:58 +0100

    WS-08: Clean up request model diff

--- backend/Models/Request/CreateSightingRequest.cs
+++ backend/Models/Request/CreateSightingRequest.cs
@@ -1,23 +1,29 @@
 using System.ComponentModel.DataAnnotations;
 using System;
-using WhaleSpotting.Models.Database;
 
 namespace WhaleSpotting.Models.Request
 {
     public class CreateSightingRequest
     {
-        public int SpeciesId { get; set; }
         [Required]
         [StringLength(70)]
         public string SeenBy { get; set; }
+
         [Required]
         public DateTime SeenOn { get; set; }
-        public string ImageUrl { get; set; }
-        public string Description { get; set; }
-        public int WhaleCount { get; set; }          
+
         [Required]
         public float Latitude { get; set; }
+
         [Required]
         public float Longitude { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int WhaleCount { get; set; }
+
+        public int SpeciesId { get; set; }
     }
 }
