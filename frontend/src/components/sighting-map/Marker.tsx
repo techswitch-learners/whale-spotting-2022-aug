0310a88eacd2df0d1ba1adb601dc3b37b16f67bc
Nick Osman <Nick.Osman@softwire.com>
2022-10-27 11:21:43 +0100

    WS-80: Removed text, tidied up

--- frontend/src/components/sighting-map/Marker.tsx
+++ frontend/src/components/sighting-map/Marker.tsx
@@ -1,20 +1,7 @@
-﻿import React from "react";
-
-interface MarkerProps {
-  text: string;
-  lat: number;
-  lng: number;
-}
-
-export const Marker = ({ id, title }: any) => {
+﻿export const Marker = ({ id, title }: any) => {
   return (
     <div className="sightings-map-pin">
       <img src="./logo.png" />
-      <p>{title}</p>
     </div>
   );
 };
-
-// export const Marker: React.FC<MarkerProps> = ({ text }) => {
-//   return <div>{text}</div>;
-// };
