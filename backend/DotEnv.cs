f52715780ffac2c847dcb73a7c49a99970cab202
Konstantinos <kon.xen@gmail.com>
2022-11-01 16:44:10 +0000

    WS-126: Now loads Envs from .env file.

---
+++ backend/DotEnv.cs
@@ -0,0 +1,28 @@
+namespace WhaleSpotting
+{
+    using System;
+    using System.IO;
+
+    public static class DotEnv
+    {
+        public static void Load(string filePath)
+        {
+           
+            if (!File.Exists(filePath))
+                throw new Exception("incorrect path"); 
+            
+            foreach (var line in File.ReadAllLines(filePath))
+            {
+                var parts = line.Split(
+                    '=',
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length != 2)
+                    continue;
+
+                Environment.SetEnvironmentVariable(parts[0], parts[1]);
+            }
+        }
+    }
+
+}
